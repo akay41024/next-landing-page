68fadb93645493168fb528d0578fd52470edf0c5
Aasif khan <akay41024@gmail.com>
2024-06-06 22:34:35 +0530

    contact us font change for shorts devices

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,7 +12,7 @@ export default function Contact() {
       <div className="min-h-screen bg-gray-100 dark:bg-neutral-900 py-12 pt-28 relative">
         <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
         <div className="max-w-2xl mx-auto p-4 relative z-10">
-          <h1 className="text-lg md:text-5xl text-center font-sans font-bold mb-8 text-white">
+          <h1 className="text-2xl md:text-5xl text-center font-sans font-bold mb-8 text-white">
             Contact Us
           </h1>
           <p className="text-neutral-300 max-w-lg mx-auto text-lg">
