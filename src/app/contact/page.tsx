54c0a00da00887845c5cd233a00e99d31f47e1e3
Aasif khan <akay41024@gmail.com>
2024-06-06 22:29:45 +0530

    some changes

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,22 +1,58 @@
-'use client'
-import React from 'react'
-import { Meteors } from "@/components/ui/meteors";
+"use client";
+import React, {useState} from "react";
 import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Contact() {
+
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
   return (
-    <div className='min-h-screen bg-neutral-900 py-12 pt-36'>
-      <h1 className="text-lg md:text-5xl text-center font-sans font-bold text-white">Contact Us</h1>
-      <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-          We&apos;re here to help with any questions about courses, program, or events. Reach out and let us know how we can assist you in your musical journey.
-      </p>
-      <form action="https://getform.io/f/wbrkzgqa" method="POST" className='flex flex-col items-center justify-center gap-2 mt-6'>
-        <input type="text" className='rounded-xl p-4 w-[40rem] bg-black text-base text-gray-600' placeholder='Your email address' />
-        <textarea name="textarea" className='rounded-xl p-4 w-[40rem] h-[10rem] bg-black text-base text-gray-600' placeholder='Your message' id="textarea" cols={10} rows={10}></textarea>
-        <button type="submit" className='p-4 bg-teal-800 rounded-2xl w-36 hover:bg-teal-600 relative right-60'>Send Message</button>
-      </form>
-      <Meteors number={50} />
-      <BackgroundBeams />
-    </div>
-  )
+    <>
+      <div className="min-h-screen bg-gray-100 dark:bg-neutral-900 py-12 pt-28 relative">
+        <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
+        <div className="max-w-2xl mx-auto p-4 relative z-10">
+          <h1 className="text-lg md:text-5xl text-center font-sans font-bold mb-8 text-white">
+            Contact Us
+          </h1>
+          <p className="text-neutral-300 max-w-lg mx-auto text-lg">
+            We&apos;re here to help with any questions about courses, program,
+            or events. Reach out and let us know how we can assist you in your
+            musical journey.
+          </p>
+          <form
+            name="contact-form"
+            action="https://getform.io/f/wbrkzgqa"
+            method="POST"
+            className="space-y-2 mt-4"
+          >
+            <input
+              type="text"
+              className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full lg:max-w-2xl p-4 bg-black placeholder:text-neutral-300"
+              placeholder="Your email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <textarea
+              name="textarea"
+              className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500 w-full lg:max-w-2xl p-4 bg-black placeholder:text-neutral-300"
+              placeholder="Your message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              id="textarea"
+              rows={5}
+              required
+            ></textarea>
+            <button
+              type="submit"
+              className="p-4 rounded-lg bg-teal-800 text-white font-medium hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
